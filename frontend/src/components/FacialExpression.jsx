4842e00a301407663afc928f0048156e3c540864
Shish <shishfruitwala29@gmail.com>
2026-03-01 12:10:14 +0530

    feature: detect mood

--- frontend/src/components/FacialExpression.jsx
+++ frontend/src/components/FacialExpression.jsx
@@ -2,47 +2,50 @@ import React, { useEffect, useRef } from "react";
 import * as faceapi from "face-api.js";
 export default function FacialExpression() {
   const videoRef = useRef();
-  const canvasRef = useRef();
+
+  const loadModels = async () => {
+    const MODEL_URL = "/models";
+    await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+    await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+  };
+
+  const startVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+      })
+      .catch((err) => console.error("Error accessing webcam: ", err));
+  };
+
+  const detectMood = async () => {
+    const detections = await faceapi
+      .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceExpressions();
+
+    let mostProbableExpression = 0;
+    let _expression = "";
+
+    if (!detections || detections.length === 0) {
+      console.log("No face detected");
+      return;
+    }
+
+    for (const expression of Object.keys(detections[0].expressions)) {
+      if (detections[0].expressions[expression] > mostProbableExpression) {
+        mostProbableExpression = detections[0].expressions[expression];
+        _expression = expression;
+      }
+    }
+
+    alert(_expression);
+  };
+
   useEffect(() => {
-    const loadModels = async () => {
-      const MODEL_URL = "/models";
-      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
-      await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
-    };
-
-    const startVideo = () => {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          videoRef.current.srcObject = stream;
-        })
-        .catch((err) => console.error("Error accessing webcam: ", err));
-    };
-
-    const handleVideoPlay = () => {
-      setInterval(async () => {
-        const detections = await faceapi
-          .detectAllFaces(
-            videoRef.current,
-            new faceapi.TinyFaceDetectorOptions(),
-          )
-          .withFaceExpressions();
-        const canvas = canvasRef.current;
-        const displaySize = {
-          width: videoRef.current.videoWidth,
-          height: videoRef.current.videoHeight,
-        };
-        faceapi.matchDimensions(canvas, displaySize);
-        const resized = faceapi.resizeResults(detections, displaySize);
-        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-        faceapi.draw.drawDetections(canvas, resized);
-        faceapi.draw.drawFaceExpressions(canvas, resized);
-      }, 500);
-    };
     loadModels().then(startVideo);
-    videoRef.current &&
-      videoRef.current.addEventListener("play", handleVideoPlay);
-  }, []);
+    videoRef.current && videoRef.current.addEventListener("play", detectMood);
+  }, 1000);
+
   return (
     <div style={{ position: "relative" }}>
       <video
@@ -51,16 +54,7 @@ export default function FacialExpression() {
         muted
         style={{ width: "720px", height: "560px" }}
       />
-      <canvas
-        ref={canvasRef}
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "720px",
-          height: "560px",
-        }}
-      />
+      <button onClick={detectMood}>Detect Mood</button>
     </div>
   );
 }
